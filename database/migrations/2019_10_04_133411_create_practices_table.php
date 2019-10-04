f581521c25f0a32e488ab1ac08d6e17ade6565be
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2019-10-04 16:26:36 +0200

    Created Practice, Employee models and migrations. Defined their relationhips accordingly (Practice's employees hasMany(), Employee's practice belongsTo()). Perhaps needless to say but defining the relationships properly allow us to do queries like so: App\Practice::first()->employees (fetch Practice's many employees) or App\Employee::first()->practice (fetch Employee's single belonging Practice by the foreign key)

---
+++ database/migrations/2019_10_04_133411_create_practices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePracticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->required();
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practices');
+    }
+}
---
+++ database/migrations/2019_10_04_133730_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name')->required();
+            $table->string('last_name')->required();
+            $table->bigInteger('practice_id')->unsigned();
+            $table->foreign('practice_id')->references('id')->on('practices');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
