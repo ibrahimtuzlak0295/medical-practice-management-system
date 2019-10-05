c44f7d226b09ec96831dc3bf281db7f2f70399d1
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2019-10-05 13:54:16 +0200

    Created an API endpoint for (only) listing all practices in database (unauthenticated).

---
+++ app/Http/Controllers/API/PracticeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Practice;
+
+class PracticeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Practice::all();
+    }
+}
