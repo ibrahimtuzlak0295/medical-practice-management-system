fdf0ad9b69a10c59e4305e17de85f9473b99f76a
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2019-10-06 22:23:07 +0200

    Validation / FormRequest classes for storing, updating Employee.

---
+++ app/Http/Requests/Employee/StoreEmployeeRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Employee;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'practice_id' => 'required|integer|exists:practices,id',
+            'email' => 'nullable|email',
+            'phone' => 'nullable|integer'
+        ];
+    }
+}
---
+++ app/Http/Requests/Employee/UpdateEmployeeRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Employee;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'sometimes|required|string',
+            'last_name' => 'sometimes|required|string',
+            'practice_id' => 'sometimes|required|integer|exists:practices,id',
+            'email' => 'sometimes|nullable|email',
+            'phone' => 'sometimes|nullable|integer'
+        ];
+    }
+}
