759af44f6819728ede34bd080ae01c9bb2cb03ce
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2019-10-06 22:17:51 +0200

    Made some of Employee's fields fillable for faster storing/updating. Made sure Employee's first + last name can be fetched as a single attribute (full_name or fullName).

--- app/Employee.php
+++ app/Employee.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+	protected $fillable = ['first_name', 'last_name', 'practice_id', 'email', 'phone'];
+
     public function practice()
     {
         return $this->belongsTo('App\Practice');
+    }
+
+    public function getFullNameAttribute()
+    {
+    	return $this->first_name . ' ' . $this->last_name;
     }
 }
