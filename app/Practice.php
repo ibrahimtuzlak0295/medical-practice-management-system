b447158c78d475f52849356322535d2533b9f636
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2019-10-06 22:21:14 +0200

    Practice's name, email, website fields are fillable. When fetching the logo attribute, make sure that the actual path is returned. When fetching the website attribute, make make sure that it's always prepended with http:// or https:// (if it isn't already, that is).

--- app/Practice.php
+++ app/Practice.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Practice extends Model
 {
+    protected $fillable = ['name', 'email', 'website'];
+    
     public function employees()
     {
         return $this->hasMany('App\Employee');
@@ -14,5 +17,19 @@ class Practice extends Model
     public function fieldsOfPractice()
     {
         return $this->belongsToMany('App\FieldsOfPractice');        
+    }
+
+    public function getLogoAttribute($value)
+    {
+        // The actual path from which the logo can be accessed publicly can vary (based on config)
+        if(NULL !== $value) return Storage::url($value);
+    }
+
+    public function getWebsiteAttribute($value)
+    {
+        if(NULL === $value) return NULL;
+
+        // Prepend 'http://' to the website if it's not set already (http or https). This is to make sure the website URL is treated as an external link when rendered (i.e. avoid having a link like this: https://this-app.com/practice-website.com, but instead: http://practice-website.com)
+        return preg_match('^https?\:\/\/^', $value) ? $value : 'http://' . $value;
     }
 }
