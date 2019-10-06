c6d46105245fd97c4229c5588e49aa2834221385
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2019-10-06 22:18:31 +0200

    FieldsOfPractice model has its name field fillable.

--- app/FieldsOfPractice.php
+++ app/FieldsOfPractice.php
@@ -8,6 +8,8 @@ class FieldsOfPractice extends Model
 {
     protected $table = 'fields_of_practice';
 
+    protected $fillable = ['name'];
+
     public function practices()
     {
         return $this->belongsToMany('App\Practice');
