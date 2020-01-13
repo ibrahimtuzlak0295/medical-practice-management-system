1304858ff09c22b232a20dba63f4500aa8178b76
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2020-01-13 14:40:22 +0100

    Employee seeder/factory

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Employee;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Employee::class, function (Faker $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->e164PhoneNumber,
+        'practice_id' => rand(1, 15)
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(PracticesTableSeeder::class);
         $this->call(FieldsOfPracticesTableSeeder::class);
+        $this->call(EmployeesTableSeeder::class);
     }
 }
